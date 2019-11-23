0fb487422c9618210de443368007e0eeff6ef5d1
souleima22 <souleima.ouerfelli7@gmail.com>
2019-11-23 14:56:23 +0100

    table candidat

---
+++ database/factories/AdministrateurFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Administrateur;
+use Faker\Generator as Faker;
+
+$factory->define(Administrateur::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/RecruteurFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Recruteur;
+use Faker\Generator as Faker;
+
+$factory->define(Recruteur::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
